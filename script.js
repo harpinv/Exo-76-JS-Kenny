d7aa9f28020c5f9f5d49378153b13e18a9544ed2
vince <vincent.harpin@orange.fr>
2022-11-23 13:48:49 +0100

    exercice 76

--- script.js
+++ script.js
@@ -1,7 +1,62 @@
+let maDiv = document.getElementById('zone');
+maDiv.style.backgroundColor = "red";
+maDiv.style.position = "relative";
+maDiv.style.height = "500px";
+maDiv.style.width = "50px";
 
+let p = 0;
+document.getElementById('compte').innerText = p;
 
-document.getElementById('up').addEventListener('click',function() {
+document.getElementById('up').addEventListener('click', function() {
     let t = parseInt(document.getElementById('kenny').style.top);
     t = t - 10;
+
+    if(t <= 0) {
+        t = 0;
+    }
+    console.log(t);
     document.getElementById('kenny').style.top = t + 'px';
 });
+
+document.getElementById('left').addEventListener('click',function() {
+    let t = parseInt(document.getElementById('kenny').style.left);
+    t = t - 10;
+
+    if(t <= 0) {
+        t = 0;
+    }
+
+    if(t < 50) {
+        alert("kenny est mort");
+        t = 200;
+        document.getElementById('kenny').style.top = 200 + "px";
+        p = p + 1;
+        document.getElementById('compte').innerText = p;
+    }
+
+    console.log(t);
+    document.getElementById('kenny').style.left = t + 'px';
+});
+
+document.getElementById('right').addEventListener('click',function() {
+    let t = parseInt(document.getElementById('kenny').style.left);
+    t = t + 10;
+
+    if(t >= 468) {
+        t = 468;
+    }
+    console.log(t);
+    document.getElementById('kenny').style.left = t + 'px';
+});
+
+document.getElementById('down').addEventListener('click',function() {
+    let t = parseInt(document.getElementById('kenny').style.top);
+    t = t + 10;
+
+    if(t >= 468) {
+        t = 468;
+    }
+    console.log(t);
+    document.getElementById('kenny').style.top = t + 'px';
+});
+
